6272a0ae8683bbcda4336857b5695684609f223a
ruthgrace90 <apostolruthgrace90@gmail.com>
2024-11-07 01:24:08 -0800

    Add files via upload

---
+++ APOSTOL/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ubuntu Server-PHP Deployment</title>
+</head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<body>
+
+
+<p>Ubuntu Server-PHP Deployment - Ruth Grace M. Apostol</p>
+
+<div class="container">
+	<label>Firstname</label>
+	<input type="text" name="firstname" class="form-control">
+</div>
+
+<div class="container">
+	<label>Middle</label>
+	<input type="text" name="middle" class="form-control">
+</div>
+
+<div class="container">
+	<label>Lastname</label>
+	<input type="text" name="lastname" class="form-control">
+</div>
+
+
+<div class="container">
+	<label>Lastname</label>
+	<input type="text" name="lastname" class="form-control">
+</div>
+
+<div class="container">
+	<label>Age</label>
+	<input type="text" name="age" class="form-control">
+</div>
+
+<div class="container">
+	<label>Address</label>
+	<input type="text" name="address" class="form-control">
+</div>
+<div class="container">
+	<label>Course&Section</label>
+	<input type="text" name="course&section" class="form-control">
+</div>
+
+<button class="SUBMIT">Green</button>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
