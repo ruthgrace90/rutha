e8c2175c142aeb3919bc9babfd9ba1345178259d
ruthgrace90 <apostolruthgrace90@gmail.com>
2024-11-07 01:28:27 -0800

    Update index.php

--- APOSTOL/index.php
+++ APOSTOL/index.php
@@ -48,7 +48,7 @@
 	<input type="text" name="course&section" class="form-control">
 </div>
 
-<button class="SUBMIT">Green</button>
+<button class="SUBMIT">SUBMIT</button>
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
